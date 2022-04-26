ca0d4349badd9dd69a39b3ce8892c190b7a72ca4
SmartDever02 <smart.jamesjin@gmail.com>
2022-04-25 21:35:40 -0400

    props type

--- pages/campaigns/new.tsx
+++ pages/campaigns/new.tsx
@@ -5,12 +5,12 @@ import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 import { Router } from '../../routes';
 
-const CampaignNew = (props) => {
+const CampaignNew = (props: any) => {
   const [minimumContribution, setminimumContribution] = useState(0);
   const [errorMessage, seterrorMessage] = useState('');
   const [loading, setloading] = useState(false);
 
-  const onSubmit = async (event:any ) => {
+  const onSubmit = async (event: any) => {
     event.preventDefault();
     console.log('Create Campaign: ', minimumContribution);
     setloading(true);
@@ -19,31 +19,37 @@ const CampaignNew = (props) => {
       const accounts = await web3.eth.getAccounts();
       console.log('Create Campaign: ', accounts[0]);
       await factory.methods.createCampaign(minimumContribution).send({
-        from: accounts[0]
+        from: accounts[0],
       });
       setloading(false);
       Router.pushRoute('/');
-    } catch(error:any) {
-      seterrorMessage(error.message)
-      setloading(false)
+    } catch (error: any) {
+      seterrorMessage(error.message);
+      setloading(false);
     }
-  }
+  };
 
   return (
     <Layout>
       <h3>Create a Campaign</h3>
       <Form onSubmit={onSubmit} error={!!errorMessage}>
         <Form.Field>
-          <label htmlFor="">Minimum Contribution</label>
-          <Input label="wei" labelPosition="right" value={minimumContribution}
-          onChange={event => setminimumContribution(event.target.value)}
-          type="number"></Input>
+          <label htmlFor=''>Minimum Contribution</label>
+          <Input
+            label='wei'
+            labelPosition='right'
+            value={minimumContribution}
+            onChange={(event) => setminimumContribution(event.target.value)}
+            type='number'
+          ></Input>
         </Form.Field>
         <Message error header='Oops' content={errorMessage} />
-        <Button primary loading={loading}>Create!</Button>
+        <Button primary loading={loading}>
+          Create!
+        </Button>
       </Form>
     </Layout>
-  )
-}
+  );
+};
 
 export default CampaignNew;
